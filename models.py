443b475b024027aaf4d0bba5bc718524a474eb76
flashdev53 <adevaraj53@gmail.com>
2026-02-20 18:40:39 +0530

    update models

--- models.py
+++ models.py
@@ -1,9 +1,20 @@
 from pydantic import BaseModel
+from typing import List
+
 
 class JournalEntryCreate(BaseModel):
     text: str
+
+
+class AnalysisModel(BaseModel):
+    sentiment: str
+    summary: str
+    topics: List[str]
+    struggle_detected: bool
+
 
 class JournalEntryResponse(BaseModel):
     id: str
     text: str
     created_at: str
+    analysis: AnalysisModel
