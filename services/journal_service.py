d4ef3e0539498eee2128b24a65a3da24490fe7b8
flashdev53 <adevaraj53@gmail.com>
2026-02-20 18:39:50 +0530

    update journal service

--- services/journal_service.py
+++ services/journal_service.py
@@ -1,19 +1,24 @@
 import uuid
 from datetime import datetime
 from services.database_service import DatabaseService
+from services.llm_service import LLMService
 
 
 class JournalService:
 
     def __init__(self):
         self.db = DatabaseService()
+        self.llm = LLMService()
 
     def create_entry(self, text: str):
+
+        analysis = self.llm.analyze_entry(text)
 
         new_entry = {
             "id": str(uuid.uuid4()),
             "text": text,
-            "created_at": datetime.utcnow().isoformat()
+            "created_at": datetime.utcnow().isoformat(),
+            "analysis": analysis
         }
 
         return self.db.create_entry(new_entry)
