2819bc5d782d3d790da5b2c9529e1b115a3d3ed4
flashdev53 <adevaraj53@gmail.com>
2026-02-20 18:32:14 +0530

    update journal service 01

--- services/journal_service.py
+++ services/journal_service.py
@@ -1,26 +1,25 @@
 import uuid
 from datetime import datetime
+from services.database_service import DatabaseService
 
 
 class JournalService:
 
     def __init__(self):
-        self.entries = []
+        self.db = DatabaseService()
 
     def create_entry(self, text: str):
+
         new_entry = {
             "id": str(uuid.uuid4()),
             "text": text,
             "created_at": datetime.utcnow().isoformat()
         }
-        self.entries.append(new_entry)
-        return new_entry
+
+        return self.db.create_entry(new_entry)
 
     def get_all_entries(self):
-        return self.entries
+        return self.db.get_all_entries()
 
     def get_entry_by_id(self, entry_id: str):
-        for entry in self.entries:
-            if entry["id"] == entry_id:
-                return entry
-        return None
+        return self.db.get_entry_by_id(entry_id)
